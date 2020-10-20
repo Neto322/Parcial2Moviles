601eab8e2614d9a43f1b72991bcf970e49ddb13b
Neto322 <42181276+Neto322@users.noreply.github.com>
2020-10-19 20:07:45 -0700

    A

---
+++ Proyecto/Proyecto/proyectoParcial/proyectoParcial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proyectoParcial
+//
+//  Created by Alumno on 10/19/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Menu principal"
+    }
+
+
+}
+
---
+++ Proyecto/Proyecto/proyectoParcial/proyectoParcial/segundoController.swift
@@ -0,0 +1,17 @@
+//
+//  segundoController.swift
+//  proyectoParcial
+//
+//  Created by Alumno on 10/19/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class segundoController : UIViewController
+{
+    override func viewDidLoad() {
+        self.title = "Animales"
+    }
+}
